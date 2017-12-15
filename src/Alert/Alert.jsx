3874aed0e3856cc4e433ff4532476283c91e8244
余翼 <yuyi@weidian.com>
2017-12-15 10:10:22 +0800

    [chore] lint procommit

--- src/Alert/Alert.jsx
+++ src/Alert/Alert.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 // import classNames from 'classnames'
-import "./style.scss"
+import './style.scss'
 
 export default class Alert extends React.Component {
 
